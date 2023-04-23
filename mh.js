5cfc3d7ed8b5914bfc672c1b6143ca3a1d261785
Kappratham <118532277+Kappratham@users.noreply.github.com>
2023-04-23 11:55:57 +0530

    Add files via upload

---
+++ mh.js
@@ -0,0 +1,13 @@
+
+console.log("Welcome to MH Java!");
+let q1a = 0;
+console.log(q1a);
+
+function q1() {
+  var q1a = document.getElementById("q1a1").value;
+  var q1a = document.getElementById("q1a2").value;
+  var q1a = document.getElementById("q1a3").value;
+  var q1a=document.getElementById("q1a4").value;
+  console.log(q1a);
+
+}
